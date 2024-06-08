ce5aff9c8b8cda9a70787f512c5010656993d3c6
Kritik <kritik0401@gmail.com>
2024-06-08 23:57:28 +0530

    Initiated db connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { PORT } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
 
 const app = express();
-port = 3001;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const { ObjectId } = require("mongodb");
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
 
-async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/Onboarding-Package");
-  if (mongoose.connection.readyState) console.log("DB connected");
-}
-
-main().catch((err) => console.log(err));
+connectDB();
