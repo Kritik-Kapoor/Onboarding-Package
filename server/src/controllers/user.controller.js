e9a39027f5acebe8855681805b0a7dbba53ca026
Kritik <kritik0401@gmail.com>
2024-06-13 23:40:38 +0530

    created register api

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,9 +1,29 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Success",
-  });
+  const { username, email, password } = req.body;
+  console.log(username, email, password);
+
+  if ([username, email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are mandatory");
+  }
+
+  const existedUser = await User.findOne({ email });
+  if (existedUser) {
+    throw new ApiError(409, "Email is already registered");
+  }
+
+  const user = await User.create({ username, email, password });
+
+  const createdUser = await User.findById(user._id);
+  if (createdUser) {
+    res
+      .status(201)
+      .json(new ApiResponse(200, createdUser, "User registered Successfully"));
+  }
 });
 
 export { registerUser };
